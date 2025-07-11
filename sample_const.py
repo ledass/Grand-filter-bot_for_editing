443a2dba07e35a2e52aa805723a13c3f6dd3091e
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-07-11 21:30:07 +0530

    Create sample_const.py

---
+++ sample_const.py
@@ -0,0 +1,34 @@
+HELP_MSG = """
+**You can find the bot commands here.**
+**User Commands:-**
+/help - Show this help message
+/settings - Toggle settings of Precise 
+
+Admin Commands:-
+/logs - Get logs as a file
+/group_broadcast  - to broadcast a message to all groups
+/stats - Get bot user stats
+/broadcast - Reply to a message to send that to all bot users
+/index - Start indexing a database channel (bot must be admin of the channel if that is provate channel)
+You can just forward the message from database channel for starting indexing, no need to use the /index 
+/ban - Ban a user from bot - /ban user_id
+/unban - Unban a user from bot - /unban user_id
+/filter - add manual filters 
+/filters - view filters
+/delall - delete all filters    
+id - get tg ids 
+/imdb - fetch info from imdb
+/alive - check bot alive  
+/ping - pong  
+/gfilter - to add a global filter 
+/delg - to delete a global filter
+"""
+
+HELP_KB = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("🔙 Back", callback_data="back_m"),
+        ],
+    ]
+
+
