3d111645d07a1d69495968581219952bb206574c
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-07-12 14:39:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
 from sample_const import HELP_MSG, HELP_KB, START_KB
-from sample_info import STARTMSG, HELPMSG,
+from sample_info import STARTMSG, HELPMSG
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, SUPPORT_CHAT, PROTECT_CONTENT, REQST_CHANNEL, SUPPORT_CHAT_ID, MAX_B_TN
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
