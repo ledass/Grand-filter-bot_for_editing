a4725c1c0625985bdc0b05cc139ab2250b242f0d
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-07-12 13:19:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,8 +8,8 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
-from sample_const import HELP_MSG, HELP_KB
-from sample_info import STARTMSG, HELPMSG 
+from sample_const import HELP_MSG, HELP_KB, START_KB
+from sample_info import STARTMSG, HELPMSG,
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, SUPPORT_CHAT, PROTECT_CONTENT, REQST_CHANNEL, SUPPORT_CHAT_ID, MAX_B_TN
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
@@ -401,7 +401,7 @@ async def back(bot, query):
     except Exception as e:
         LOGGER.warning(e)
         start_msg = STARTMSG
-    await query.message.edit_text(start_msg, reply_markup=START_TXT)
+    await query.message.edit_text(start_msg, reply_markup=START_KB)
     
 
 
